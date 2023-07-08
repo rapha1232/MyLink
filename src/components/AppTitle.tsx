cf1e09920ad392e754fc5a55f6148827eb412170
raphy1232 <raphy1232@gmail.com>
2023-07-08 15:57:01 +0300

    changed title font

--- src/components/AppTitle.tsx
+++ src/components/AppTitle.tsx
@@ -1,3 +1,3 @@
 export default function AppTitle() {
-  return <h1 className="mb-10 text-center text-6xl text-white">My Link App</h1>;
+  return <h1 className="mb-10 text-center text-6xl text-white font-sans">My Link App</h1>;
 }
